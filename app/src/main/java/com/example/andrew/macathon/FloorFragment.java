ddbee03c39ddb58a6d4d700b26c823e15d5a77c1
danyshi <danyshi2002@gmail.com>
2015-02-20 20:48:46 -0600

    layouts
    layouts

--- app/src/main/java/com/example/andrew/macathon/FloorFragment.java
+++ app/src/main/java/com/example/andrew/macathon/FloorFragment.java
@@ -1,8 +1,29 @@
 package com.example.andrew.macathon;
 
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
 /**
  * Created by andrew on 2/20/15.
  * Manages the collection of rooms and updates their appearance based on the current time
  */
-public class FloorFragment {
+public class FloorFragment extends Fragment {
+
+
+
+    ListView listView = (ListView) findViewById(R.id.floor_view);
+    listView.setAdapter(adapter);
+
+    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+            android.R.layout.floor_view, room_array);
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 }
